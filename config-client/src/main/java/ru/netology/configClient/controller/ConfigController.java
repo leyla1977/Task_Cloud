7fc22d81dafdcf6700cd17b00d21637bc13ec3d4
elvira egorova <leylaazbuhan@gmail.com>
2026-01-13 03:00:19 +0300

    first commit

---
+++ config-client/src/main/java/ru/netology/configClient/controller/ConfigController.java
@@ -0,0 +1,42 @@
+package ru.netology.configClient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    // Добавьте значения по умолчанию через двоеточие
+    @Value("${my.custom.property:Default Property Value}")
+    private String customProperty;
+
+    @Value("${my.custom.version:0.0.0}")
+    private String version;
+
+    @Value("${custom.message:Default Message}")
+    private String customMessage;
+
+    @GetMapping("/config")
+    public String getConfig() {
+        return String.format("""
+            Configuration from Config Server:
+            Property: %s
+            Version: %s
+            Message: %s
+            """, customProperty, version, customMessage);
+    }
+
+    @GetMapping("/property")
+    public String getProperty() {
+        return customProperty;
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return customMessage;
+    }
+}
